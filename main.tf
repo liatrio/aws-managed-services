fb2ed912a6e55f6c04eb999fb3e3058d09d28fc7
Paul Henson <paulh@liatrio.com>
2023-06-08 14:47:24 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,147 @@
+locals {
+  #name        = local.locals_vars.locals.name
+  description = "Amazon Managed Grafana workspace for ${local.name}"
+
+  tags = {
+    GithubRepo = "terraform-aws-observability-accelerator"
+    GithubOrg  = "aws-observability"
+  }
+  grafana_iam_role_name = module.managed_grafana.workspace_iam_role_name
+}
+
+
+resource "aws_prometheus_workspace" "this" {
+  count = var.enable_managed_prometheus ? 1 : 0
+
+  alias = local.name
+  tags  = var.tags
+}
+
+resource "aws_prometheus_alert_manager_definition" "this" {
+  count = var.enable_alertmanager ? 1 : 0
+
+  workspace_id = local.amp_ws_id
+
+  definition = <<EOF
+alertmanager_config: |
+    route:
+      receiver: 'default'
+    receivers:
+      - name: 'default'
+EOF
+}
+# TODO: Tweak permissions to be least privileged.
+provider "grafana" {
+  url  = local.amg_ws_endpoint
+  auth = module.managed_grafana.workspace_api_keys["admin"].key
+}
+
+resource "grafana_data_source" "amp" {
+  count      = var.create_prometheus_data_source ? 1 : 0
+  type       = "prometheus"
+  name       = local.name
+  is_default = true
+  url        = local.amp_ws_endpoint
+  json_data {
+    http_method     = "GET"
+    sigv4_auth      = true
+    sigv4_auth_type = "workspace-iam-role"
+    sigv4_region    = local.amp_ws_region
+  }
+}
+
+#dashboards
+resource "grafana_folder" "this" {
+  count = var.create_dashboard_folder ? 1 : 0
+  title = "Observability Accelerator Dashboards"
+}
+
+module "managed_prometheus" {
+  source = "git@github.com:liatrio/managed-prometheus-monitoring.git?ref=prometheus"
+  #source = "/Users/paulhenson/liatrio/repos/managed-prometheus-monitoring"
+  #version = "0.0.1"
+
+  aws_region                      = local.amp_ws_region
+  dashboards_folder_id            = grafana_folder.this[0].id
+  managed_prometheus_workspace_ids = aws_prometheus_workspace.this[0].id
+  active_series_threshold         = 100000
+  ingestion_rate_threshold        = 70000
+}
+
+# fetch user (could also be a group https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/identitystore_group)
+# the instance has to be fetched first and it doesn't require any arguments
+data "aws_ssoadmin_instances" "instances" {}
+data "aws_identitystore_user" "user" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.instances.identity_store_ids)[0]
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "UserName"
+      attribute_value = "o11yTestUser" #TODO: Change this to be a variable
+    }
+  }
+}
+
+module "managed_grafana" {
+  source  = "terraform-aws-modules/managed-service-grafana/aws"
+  version = "1.8.0"
+
+  name                      = local.name
+  associate_license         = false
+  description               = local.description
+  account_access_type       = "CURRENT_ACCOUNT"
+  authentication_providers  = ["AWS_SSO"]
+  permission_type           = "SERVICE_MANAGED"
+  data_sources              = ["CLOUDWATCH", "PROMETHEUS", "XRAY"]
+  notification_destinations = ["SNS"]
+  stack_set_name            = local.name
+  create                    = var.create
+  create_workspace          = var.create_workspace
+
+  # Workspace API keys
+  workspace_api_keys = {
+    # viewer = {
+    #   key_name        = "viewer"
+    #   key_role        = "VIEWER"
+    #   seconds_to_live = 3600
+    # }
+    # editor = {
+    #   key_name        = "editor"
+    #   key_role        = "EDITOR"
+    #   seconds_to_live = 3600
+    # }
+    admin = {
+      key_name        = "admin"
+      key_role        = "ADMIN"
+      seconds_to_live = 2592000
+    }
+  }
+
+  configuration = jsonencode({
+    unifiedAlerting = {
+      enabled = true
+    }
+  })
+
+  role_associations = {
+    "ADMIN" = {
+      "user_ids" = [data.aws_identitystore_user.user.user_id]
+    }
+    # "EDITOR" = {
+    #   "user_ids" = ["2222222222-abcdefgh-1234-5678-abcd-999999999999"]
+    # }
+  }
+
+
+  # Workspace IAM role
+  create_iam_role                = true
+  iam_role_name                  = "workspace-iam-role"
+  use_iam_role_name_prefix       = true
+  iam_role_description           = local.description
+  iam_role_path                  = "/grafana/"
+  iam_role_force_detach_policies = true
+  iam_role_max_session_duration  = 7200
+  iam_role_tags                  = local.tags
+
+  tags = local.tags
+}
---
+++ outputs.tf
@@ -0,0 +1,59 @@
+output "aws_region" {
+  description = "AWS Region"
+  value       = var.aws_region
+}
+
+output "managed_prometheus_workspace_endpoint" {
+  description = "Amazon Managed Prometheus workspace endpoint"
+  value       = local.amp_ws_endpoint
+}
+
+output "managed_prometheus_workspace_id" {
+  description = "Amazon Managed Prometheus workspace ID"
+  value       = local.amp_ws_id
+}
+
+output "managed_prometheus_workspace_region" {
+  description = "Amazon Managed Prometheus workspace region"
+  value       = local.amp_ws_region
+}
+
+output "managed_grafana_workspace_endpoint" {
+  description = "Amazon Managed Grafana workspace endpoint"
+  value       = local.amg_ws_endpoint
+}
+
+output "managed_grafana_workspace_id" {
+  description = "Amazon Managed Grafana workspace ID"
+  value       = local.amg_ws_id
+}
+
+output "grafana_dashboards_folder_id" {
+  description = "Grafana folder ID for automatic dashboards. Required by workload modules"
+  value       = var.create_dashboard_folder ? grafana_folder.this[0].id : ""
+}
+
+output "grafana_prometheus_datasource_test" {
+  description = "Grafana save & test URL for Amazon Managed Prometheus workspace"
+  value       = var.create_prometheus_data_source ? "${local.amg_ws_endpoint}/datasources/edit/${grafana_data_source.amp[0].uid}" : ""
+}
+
+output "grafana_dashboard_folder_created" {
+  description = "Boolean value indicating if the module created a dashboard folder in Amazon Managed Grafana"
+  value       = var.create_dashboard_folder
+}
+
+output "prometheus_data_source_created" {
+  description = "Boolean value indicating if the module created a prometheus data source in Amazon Managed Grafana"
+  value       = var.create_prometheus_data_source
+}
+
+output "create" {
+  description = "The creatae flag that gets passed to the module."
+  value       = var.create
+}
+
+output "create_workspace" {
+  description = "The create_workspace flag that gets passed to the module."
+  value       = var.create_workspace
+}
