2685d3727f6faa467fabf8fd6560097db20644e2
PaulHLiatrio <100874415+PaulHLiatrio@users.noreply.github.com>
2023-06-26 13:20:18 -0700

    refactor: remove prometheus module (#10)

--- locals.tf
+++ locals.tf
@@ -8,9 +8,9 @@ data "aws_grafana_workspace" "this" {
 
 locals {
   # if region is not passed, we assume the current one
-  amp_ws_region   = coalesce(var.managed_prometheus_workspace_region, data.aws_region.current.name)
-  amp_ws_id       = var.enable_managed_prometheus ? aws_prometheus_workspace.this[0].id : var.managed_prometheus_workspace_id
-  amp_ws_endpoint = "https://aps-workspaces.${local.amp_ws_region}.amazonaws.com/workspaces/${local.amp_ws_id}/"
+  # amp_ws_region   = coalesce(var.managed_prometheus_workspace_region, data.aws_region.current.name)
+  # amp_ws_id       = var.enable_managed_prometheus ? aws_prometheus_workspace.this[0].id : var.managed_prometheus_workspace_id
+  # amp_ws_endpoint = "https://aps-workspaces.${local.amp_ws_region}.amazonaws.com/workspaces/${local.amp_ws_id}/"
 
   amg_ws_endpoint = "https://${data.aws_grafana_workspace.this.endpoint}"
   amg_ws_id       = module.managed_grafana.workspace_id
