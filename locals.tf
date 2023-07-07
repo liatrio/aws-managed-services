9655eee00661ac960038bd10afe144ce5919b26d
Anthony Gauthier <antho325@hotmail.com>
2023-07-07 16:13:52 -0400

    refactor: added a couple locals and made sure variables are available for AMP

--- locals.tf
+++ locals.tf
@@ -11,8 +11,9 @@ locals {
   iam_role_name         = "aws-observability-workspace-iam-role"
   amg_ws_endpoint       = "https://${data.aws_grafana_workspace.this.endpoint}"
   amg_ws_id             = module.managed_grafana.workspace_id
-
-  amp_ws_region         = var.aws_region
+  
+  amp_ws_alias  = "observability-amp-workspace"
+  amp_ws_region = var.aws_region
 
   grafana_workspace_id = data.aws_grafana_workspace.this.workspace_id
 }
--- prometheus.tf
+++ prometheus.tf
@@ -2,11 +2,15 @@
 #   title = "Amazon Managed Prometheus monitoring dashboards"
 # }
 
+resource "aws_prometheus_workspace" "amp_ws" {
+  count = var.enable_managed_prometheus == true ? 1 : 0
+  alias = local.amp_ws_alias
+}
 
 ## TODO: Moved this where it makes sense, currently in this file/folder for ease of iterating
 module "managed_prometheus" {
+  count = var.enable_managed_prometheus == true ? 1 : 0
   source                           = "github.com/liatrio/terraform-aws-managed-service-prometheus.git"
-#   dashboards_folder_id             = resource.grafana_folder.this.id
   aws_region                       = local.amp_ws_region
-  managed_prometheus_workspace_ids = var.managed_prometheus_workspace_ids
+  managed_prometheus_workspace_ids = list(resource.amp_ws.id)
 }
