4bed182e77e70c33d0c9ed28907912e3f1af094a
Anthony Gauthier <antho325@hotmail.com>
2023-07-13 09:19:53 -0400

    feat: added aws_iam_role for prometheus

--- prometheus.tf
+++ prometheus.tf
@@ -7,6 +7,32 @@ resource "aws_prometheus_workspace" "amp_ws" {
   alias = local.amp_ws_alias
 }
 
+resource "aws_iam_role" "amp_iam_role" {
+  name = "amp_iam_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+          "aps:RemoteWrite", 
+          "aps:GetSeries", 
+          "aps:GetLabels",
+          "aps:GetMetricMetadata"
+      ], 
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    description = "IAM Role used by prometheus on EKS to interact with AMP"
+  }
+}
+
 ## TODO: Moved this where it makes sense, currently in this file/folder for ease of iterating
 module "managed_prometheus" {
   count = var.enable_managed_prometheus == true ? 1 : 0
