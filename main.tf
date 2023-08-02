c842c457dc6061769dcc63b6c7f7a6e2da0c857c
Paul Henson <paulh@liatrio.com>
2023-08-02 13:45:30 -0500

    feat: added aws_kms_key resource to change primary key used for encryption away from default

--- main.tf
+++ main.tf
@@ -75,6 +75,11 @@ module "managed_grafana" {
 
 resource "aws_secretsmanager_secret" "grafana_api_token" {
   name = "AMG_API_Token"
+  kms_key_id = aws_kms_key.secrets.arn
+}
+
+resource "aws_kms_key" "secrets" {
+    enable_key_rotation = true
 }
 
 resource "aws_secretsmanager_secret_version" "sversion" {
