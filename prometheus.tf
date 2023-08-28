e65bf4427b8feaf5fa634dc32d566c8a51c0ab01
Paul Henson <paulh@liatrio.com>
2023-08-28 14:08:10 -0500

    fix: adjusted workspace ID

--- prometheus.tf
+++ prometheus.tf
@@ -6,7 +6,7 @@ resource "aws_prometheus_workspace" "amp_ws" {
 resource "aws_prometheus_alert_manager_definition" "this" {
   count = var.enable_alertmanager ? 1 : 0
 
-  workspace_id = local.amp_ws_id
+  workspace_id = aws_prometheus_workspace.amp_ws.id
 
   definition = <<EOF
 alertmanager_config: |
@@ -19,7 +19,7 @@ EOF
 
 resource "aws_prometheus_rule_group_namespace" "demo" {
   name         = "rules"
-  workspace_id = aws_prometheus_workspace.demo.id
+  workspace_id = aws_prometheus_workspace.amp_ws.id
   data         = file("./rules.yml")
 }
 
