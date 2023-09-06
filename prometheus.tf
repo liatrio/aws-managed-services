67cc1c13e59a487b9f51a73290d2731bd33951fd
Paul Henson <paulh@liatrio.com>
2023-09-06 16:02:28 -0500

    fix: prometheus-logging added logging to prometheus workspace

--- prometheus.tf
+++ prometheus.tf
@@ -1,6 +1,15 @@
+# TODO: We are currently flowing data through this workspace here, we will eventually want to move everything to the workspace being created by the public module.
 resource "aws_prometheus_workspace" "amp_ws" {
   count = var.enable_managed_prometheus == true ? 1 : 0
   alias = var.amp_ws_alias
+
+  dynamic "logging_configuration" {
+    for_each = length(var.logging_configuration) > 0 ? [var.logging_configuration] : []
+
+    content {
+      log_group_arn = logging_configuration.value.log_group_arn
+    }
+  }
 }
 
 resource "aws_prometheus_alert_manager_definition" "this" {
@@ -63,6 +72,7 @@ module "managed_prometheus" {
 
   create_workspace = var.amp_create_workspace == true ? var.amp_create_workspace : false
   workspace_id     = var.amp_workspace_id
+  create = true
 
   logging_configuration = var.logging_configuration
 }
