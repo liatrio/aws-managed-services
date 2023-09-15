65c531ae518ca712c77d3b6b6912b49ab143e205
Patrick Paulino <patrickp@liatrio.com>
2023-09-15 12:57:44 -0700

    fix: add bucket sse security

--- main.tf
+++ main.tf
@@ -124,20 +124,36 @@ resource "aws_route53_zone" "private" {
     content {
       vpc_id = vpc.value
     }
-    lifecycle {
-      ignore_changes = [
-        # Ignore changes to vpcs, e.g. because a management agent
-        # updates these based on some ruleset managed elsewhere.
-        vpc,
-      ]
-    }
+  }
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to vpcs, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      vpc,
+    ]
   }
 
   tags = var.aws_route53_zone_tags
 }
 
+resource "aws_kms_key" "amg_bucket_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "amg_bucket" {
   bucket = "grafana.${var.route53_hosted_zone_name}"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "amg_bucket" {
+  bucket = aws_s3_bucket.amg_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.amg_bucket_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 resource "aws_s3_bucket_ownership_controls" "amg_bucket" {
