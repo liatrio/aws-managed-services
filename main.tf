8c32ec0f1def700abbcbd7607c9c333ba848bca4
Patrick Paulino <pmpaulino@gmail.com>
2023-09-15 11:29:57 -0700

    feat: add s3 static site redirect for grafana (#46)
    * feat: add s3 static site redirect for grafana
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -115,3 +115,40 @@ provider "grafana" {
   url  = local.amg_ws_endpoint
   auth = module.managed_grafana.workspace_api_keys["admin"].key
 }
+
+resource "aws_route53_zone" "private" {
+  name = var.route53_hosted_zone_name
+
+  dynamic "vpc" {
+    for_each = var.vpc_ids
+    content {
+      vpc_id = vpc.value
+    }
+  }
+
+  tags = var.aws_route53_zone_tags
+}
+
+resource "aws_s3_bucket" "amg_bucket" {
+  bucket = "grafana.${var.route53_hosted_zone_name}"
+}
+
+resource "aws_s3_bucket_website_configuration" "amg_bucket_website" {
+  bucket = aws_s3_bucket.amg_bucket.bucket
+
+  redirect_all_requests_to {
+    host_name = var.amg_redirect_hostname
+  }
+}
+
+resource "aws_route53_record" "s3_alias" {
+  name    = aws_s3_bucket.amg_bucket.bucket
+  type    = "A"
+  zone_id = aws_route53_zone.private.zone_id
+
+  alias {
+    name                   = aws_s3_bucket_website_configuration.amg_bucket_website.website_endpoint
+    zone_id                = local.s3_website_endpoint_zone_id
+    evaluate_target_health = false
+  }
+}
