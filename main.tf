8dd5e327a5dc78e67afd04aabd71c5582cc157eb
Patrick Paulino <patrickp@liatrio.com>
2023-09-15 13:17:13 -0700

    fix: tfsec failures for s3 bucket security

--- main.tf
+++ main.tf
@@ -155,6 +155,7 @@ resource "aws_s3_bucket" "amg_bucket" {
 resource "aws_s3_bucket_public_access_block" "amg_bucket" {
   bucket                  = aws_s3_bucket.amg_bucket.id
   block_public_acls       = true
+  block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
