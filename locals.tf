118b1310e0509a78741befc824241dd87d90cc22
Paul Henson <paulh@liatrio.com>
2023-09-28 14:04:44 -0500

    refactor: fixed formatting

--- locals.tf
+++ locals.tf
@@ -5,10 +5,10 @@ locals {
   grafana_iam_role_name = module.managed_grafana.workspace_iam_role_name
   iam_role_name         = "aws-observability-workspace-iam-role"
 
-  amp_ws_endpoint       = module.managed_grafana.outputs.managed_prometheus_workspace_endpoint
-  amg_ws_endpoint       = "https://${data.aws_grafana_workspace.this.endpoint}"
-  amg_ws_id             = module.managed_grafana.workspace_id
-  idp_metadata_url      = var.generate_metadata_url ? "${var.idp_url_with_postfix}${data.aws_grafana_workspace.this.endpoint}/saml/metadata" : ""
+  amp_ws_endpoint  = module.managed_grafana.outputs.managed_prometheus_workspace_endpoint
+  amg_ws_endpoint  = "https://${data.aws_grafana_workspace.this.endpoint}"
+  amg_ws_id        = module.managed_grafana.workspace_id
+  idp_metadata_url = var.generate_metadata_url ? "${var.idp_url_with_postfix}${data.aws_grafana_workspace.this.endpoint}/saml/metadata" : ""
 
   grafana_workspace_id = data.aws_grafana_workspace.this.workspace_id
 }
