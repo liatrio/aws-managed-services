522d953a05627dbda9feb1d67b93313c9275483e
Anthony Gauthier <antho325@hotmail.com>
2023-10-03 14:29:05 -0400

    fix(amp): added default cw_log_group for AMP (#59)
    * fix(amp): added default cw_log_group for AMP
    
    * terraform-docs: automated action
    
    * chore(amp): renamed variable
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: Anthony Gauthier <Anthony.Gauthier@wnco.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- prometheus.tf
+++ prometheus.tf
@@ -1,14 +1,27 @@
+resource "aws_cloudwatch_log_group" "amp_log_group" {
+  name = "o11y-amp-default"
+}
+
 # TODO: We are currently flowing data through this workspace here, we will eventually want to move everything to the workspace being created by the public module.
 resource "aws_prometheus_workspace" "amp_ws" {
   count = var.enable_managed_prometheus == true ? 1 : 0
   alias = var.amp_ws_alias
 
-  dynamic "logging_configuration" {
-    for_each = length(var.logging_configuration) > 0 ? [var.logging_configuration] : []
+  # NOTE:
+  # Currently commented this out in favour of a default log group (defined above).
+  # Avoids having to manually define log groups prior to executingthe terraform module.
+  #
+  # ---
+  # dynamic "logging_configuration" {
+  #   for_each = length(var.logging_configuration) > 0 ? [var.logging_configuration] : []
 
-    content {
-      log_group_arn = logging_configuration.value.log_group_arn
-    }
+  #   content {
+  #     log_group_arn = logging_configuration.value.log_group_arn
+  #   }
+  # }
+
+  logging_configuration {
+    log_group_arn = aws_cloudwatch_log_group.amp_log_group.arn
   }
 }
 
